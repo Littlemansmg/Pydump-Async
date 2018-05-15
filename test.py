07301f8ce05f1f40c90bc4c6bb61189287d774f7
LittlemanSMG <littlemansmg@gmail.com>
2018-05-15 17:46:54 -0500

    aiohttp
    Implemented in test

--- test.py
+++ test.py
@@ -1,16 +1,44 @@
+import aiohttp
+import json
+import discord
+import asyncio
+from datetime import datetime as dt
 import time
-import urllib.request, json
 
 reddit = input('Name of subreddit. Ex. r/dankmemes: ')
 sort = input('Sorted by? New/Top/Hot: ')
 
+
+
 url = f'https://www.reddit.com/r/{reddit}/{sort.strip()}/.json'
 
-try:
-    with urllib.request.urlopen(url) as post:
-        data = json.loads(post.read().decode())
-    print(data['data']['children'][0]['data']['url'])
+async def getpost():
+    posts = []
+    now = dt.utcnow()
+    images = []
+    try:
+        with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                if resp.status == 200:
+                    json = await resp.json()
+                    posts = json['data']['children']
+                    posts = list(map(lambda p: p['data'], posts))
+        for x in posts:
+            posttime = dt.utcfromtimestamp(x['created_utc'])
+            if (((now - posttime).total_seconds()) / 300) <= 1:
+            #if x['created_utc'] <= int(now) + 14100:
+                images.append(x['url'])
+            else:
+                return images
+    except Exception as e:
+        print(e)
+    finally:
+        return images
 
-except:
-    print('Sorry, can\'t get to that reddit or it doesn\'t exist.')
+async def display():
+    images = await getpost()
+    print(images)
 
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(display())
