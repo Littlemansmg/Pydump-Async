d4c73d921bece6796ee5c52a9adff2f1c9d83d62
LittlemanSMG <littlemansmg@gmail.com>
2018-05-15 09:31:54 -0500

    Reddit Test w/o aiohttp

--- Pydump.py
+++ Pydump.py
@@ -1,9 +1,16 @@
 import time
 import urllib.request, json
 
-url = 'https://www.reddit.com/r/dankmemes/new/.json'
+reddit = input('Name of subreddit. Ex. r/dankmemes: ')
+sort = input('Sorted by? New/Top/Hot: ')
 
-with urllib.request.urlopen(url) as post:
-    data = json.loads(post)['data']['children']['data']
+url = f'https://www.reddit.com/r/{reddit}/{sort.strip()}/.json'
 
-print (data)
+try:
+    with urllib.request.urlopen(url) as post:
+        data = json.loads(post.read().decode())
+    print(data['data']['children'][0]['data']['url'])
+
+except:
+    print('Sorry, can\'t get to that reddit or it doesn\'t exist.')
+
--- Webhook_test.py
+++ Webhook_test.py
@@ -2,6 +2,7 @@ import logging
 import aiohttp
 import discord
 from discord.ext import commands
+import urllib.request, json
 
 with open('token.txt') as token:
     token = token.readline()
@@ -10,61 +11,19 @@ bot = commands.Bot(command_prefix = '*')
 
 @bot.event
 async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('----------')
-
-log = logging.getLogger('discord')
-
-fancy_name = "Reddit"
-
-message_format = "`New post from /r/{subreddit}`\n\n"\
-                 "**{title}** *by {author}*\n"\
-                 "{content}\n"\
-                 "**Link** {link} \n \n"
-
-async def get_posts(self, subreddit):
-    """Gets the n last posts of a subreddit
-    Args:
-        subreddit: Subbredit name
-        n: The number of posts you want
-    Returns:
-        A list of posts
-    """
-
-    url = "https://www.reddit.com/r/{}/new.json".format(subreddit)
-    posts = []
-
-    try:
-        with aiohttp.ClientSession() as session:
-            async with session.get(url) as resp:
-                if resp.status == 200:
-                    json = await resp.json()
-                    posts = json['data']['children']
-                    posts = list(map(lambda p: p['data'], posts))
-    except Exception as e:
-        log.info("Cannot get posts from {}".format(subreddit))
-        log.info(e)
-        return []
-
-    return posts[:2]
-
-async def display_posts(posts):
-    """Display a list of posts into the corresponding destination channel.
-    This function only displays posts that hasn't been posted previously.
-    """
-    for subreddit, posts in posts.items():
-        subreddit_posts = reversed(posts)
-        for post in subreddit_posts:
-
-            selftext = post['selftext'] or ""
-            message = "http://redd.it/"+post['id']
-    return message
+    bot.change_presence(game=discord.Game(name='Type *help for help'))
 
 @bot.command(pass_context = True, name = 'get')
-async def getPosts(ctx):
-    destination = ctx.message.channel
-    bot.send_message(destination, display_posts(get_posts(None, 'dankmemes')))
+async def getPosts(ctx, *, reddit, sort):
+
+    url = f'https://www.reddit.com/r/{reddit}/{sort.strip()}/.json'
+
+    try:
+        with urllib.request.urlopen(url) as post:
+            data = json.loads(post.read().decode())
+        bot.say(data['data']['children'][0]['data']['url'])
+
+    except:
+        bot.say('Sorry, can\'t get to that reddit or it doesn\'t exist.')
 
 bot.run(token.strip())
