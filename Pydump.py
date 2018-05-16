ac8e005634800d01a2cc7f579c45f3504e5c7ef5
LittlemanSMG <littlemansmg@gmail.com>
2018-05-16 12:13:42 -0500

    No way this works first try

--- Pydump.py
+++ Pydump.py
@@ -2,39 +2,54 @@ import logging
 import aiohttp
 import discord
 import asyncio
-from discord import server
 from discord.ext import commands
 from datetime import datetime as dt
 
 with open('token.txt') as token:
     token = token.readline()
 
+# settings
+settings = {
+    '0': {
+        'id': '431217188410490891',
+        'create_channels': 0,
+        'default_channel': 'general',
+        'watching': ['memes', 'dankmemes', 'curledfeetsies']
+    }
+}
+
 async def getposts():
     await bot.wait_until_ready()
 
-    destination = discord.utils.get(bot.get_all_channels(), name = 'general')
-    url = 'https://www.reddit.com/r/ironicsigns/new/.json'
-    posts = []
-    now = dt.utcnow()
-    images = []
-    try:
-        with aiohttp.ClientSession() as session:
-            async with session.get(url) as resp:
-                if resp.status == 200:
-                    json = await resp.json()
-                    posts = json['data']['children']
-                    posts = list(map(lambda p: p['data'], posts))
-        for x in posts:
-            posttime = dt.utcfromtimestamp(x['created_utc'])
-            if (((now - posttime).total_seconds()) / 300) <= 1:
-                # if x['created_utc'] <= int(now) + 14100:
-                images.append(x['url'])
+    for id in settings:
+        destination = discord.utils.get(
+            bot.get_all_channels(),
+            server_id = settings[id]['id'],
+            name = settings[id]['default_channel'])
+        reddits = list(settings[id]['watching'])
+        for reddit in reddits:
+            url = f"https://www.reddit.com/r/{reddit}/new/.json"
+            posts = []
+            now = dt.utcnow()
+            images = []
+            try:
+                with aiohttp.ClientSession() as session:
+                    async with session.get(url) as resp:
+                        if resp.status == 200:
+                            json = await resp.json()
+                            posts = json['data']['children']
+                            posts = list(map(lambda p: p['data'], posts))
+                for x in posts:
+                    posttime = dt.utcfromtimestamp(x['created_utc'])
+                    if (((now - posttime).total_seconds()) / 300) <= 1:
+                        # if x['created_utc'] <= int(now) + 14100:
+                        images.append(x['url'])
 
-        for image in images:
-            await bot.send_message(destination, image)
+                for image in images:
+                    await bot.send_message(destination, image)
 
-    except Exception as e:
-        print(e)
+            except Exception as e:
+                print(e)
 
     await asyncio.sleep(300)
 
