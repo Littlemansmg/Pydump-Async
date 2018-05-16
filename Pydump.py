0be092df2ee66433d2562a3ffa89bf28573e22aa
LittlemanSMG <littlemansmg@gmail.com>
2018-05-16 13:07:04 -0500

    server id to int

--- Pydump.py
+++ Pydump.py
@@ -34,7 +34,7 @@ async def getposts():
 
             if not destination:
                 # bot.get_guild(
-                await bot.send_message(bot.get_server(settings[id]['id']), "I don't have a default channel to post in!"
+                await bot.send_message(bot.get_server(int(settings[id]['id'])), "I don't have a default channel to post in!"
                                                                 "please type `*default_channel` to set it!")
                 break
             reddits = list(settings[id]['watching'])
