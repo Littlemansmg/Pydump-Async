99155614e17da3bfed88d92db2045dfcdc4432ea
LittlemanSMG <littlemansmg@gmail.com>
2018-05-23 15:58:31 -0500

    changes made?

--- test.py
+++ test.py
@@ -1,46 +1,11 @@
-# import aiohttp
-# import asyncio
-# from datetime import datetime as dt
-#
-# reddit = input('Name of subreddit. Ex. r/dankmemes: ')
-# sort = input('Sorted by? New/Top/Hot: ')
-#
-# async def getpost():
-#     url = f'https://www.reddit.com/r/{reddit}/{sort.strip()}/.json'
-#     posts = []
-#     now = dt.utcnow()
-#     images = []
-#     try:
-#         with aiohttp.ClientSession() as session:
-#             async with session.get(url) as resp:
-#                 if resp.status == 200:
-#                     json = await resp.json()
-#                     posts = json['data']['children']
-#                     posts = list(map(lambda p: p['data'], posts))
-#         for x in posts:
-#             posttime = dt.utcfromtimestamp(x['created_utc'])
-#             if (((now - posttime).total_seconds()) / 300) <= 1:
-#             #if x['created_utc'] <= int(now) + 14100:
-#                 images.append(x['url'])
-#             else:
-#                 return images
-#     except Exception as e:
-#         print(e)
-#     finally:
-#         return images
-#
-# async def display():
-#     images = await getpost()
-#     print(images)
-#
-# if __name__ == '__main__':
-#     loop = asyncio.get_event_loop()
-#     loop.run_until_complete(display())
+import json
+from collections import defaultdict
 
-def test():
-    return [123]
+with open('options.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
 
-if not test():
-    print("false")
-elif test():
-    print("true")
+subs = data['0']['watching']
+subs.append('atbge')
+data['0']['watching'] = subs
+
+print(data['0']['watching'])
